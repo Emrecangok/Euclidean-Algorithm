178d973142ebaa936ac4533924c07560a0c43c08
Emrecangok <115079415+Emrecangok@users.noreply.github.com>
2023-01-13 20:30:15 +0300

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+
+int main() {
+
+	int sayilar[3];
+	printf("sayiyi giriniz\n");
+	scanf("%d",&sayilar[0]);
+	printf("diger sayiyi giriniz\n");
+	scanf("%d",&sayilar[1]);
+	if (sayilar[0]<sayilar[1])
+	{
+		sayilar[2] = sayilar[0];
+		sayilar[0] = sayilar[1];
+		sayilar[1] = sayilar[2];
+
+	}
+	//büyük ve küçük sayýyý belirledik dizinin 2. indisi yani dizi[2] gecici degerleri tutmak içindir
+
+	ihtar:
+	if (sayilar[0]%sayilar[1]==0)
+	{
+		printf("EBOB=%d",sayilar[1]);
+
+	}
+	else
+	{
+		sayilar[2]=sayilar[0]%sayilar[1];
+		sayilar[0] = sayilar[1];
+		sayilar[1] = sayilar[2];
+		goto ihtar;
+	}
+
+
+
+
+	
+
+
+
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
